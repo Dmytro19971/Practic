2f7ee48ad6b915c1f4f4760cc6bec31feddcc41f
Dmytro19971 <safonovdima2@gmail.com>
2024-09-22 08:15:58 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as f_dv
+from true_math import divide as t_dv
+result1 = f_dv(69, 3)
+result2 = f_dv(3, 0)
+result3 = t_dv(49, 7)
+result4 = t_dv(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
